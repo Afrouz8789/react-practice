cfe8a21338c6b3d5533f124adfe5accc5e4aa286
Shaik Afrouz <shaikafrouz@Shaiks-MBP.Dlink>
2022-03-26 23:37:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
-import React,{useState,useEffect} from "react";
-import MovieCard from "./MovieCard"
-import "./App.css"
-import SearchIcon from "./search.svg"
-// 74bcaa39
-const ApiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=74bcaa39'
-
-const App = () =>{
-    const [movies,setMovies] =useState([])
-    const [search,setSearch]=useState('')
-    const searchMovies = async(title) => {
-        const response= await fetch(`${ApiUrl}&s=${title}`)
-        const data= await response.json()
-        setMovies(data.Search)
-    }
-
-    useEffect(() => {
-        searchMovies('spiderman')
-    },[])
-    return <div className="app">
-        <h1>MovieLand</h1>
-        <div className="search">
-        <input 
-        placeholder="Search for Movies"
-        value={search}
-        onChange={(e) => (setSearch(e.target.value))}
-        />
-        <img
-        src={SearchIcon}
-        alt="search"
-        onClick={() => (searchMovies(search))}
-        />
-        
-        </div>
-        {
-            movies?.length >0 ? (<div className="container">
-                {movies.map((movie) => (<MovieCard movie1={movie}/>))}
-            
-            </div>) : (<div className="empty"> 
-            <h2>No movies found</h2>
-            </div>)
-        }
-        
-        
+import react from "react";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import {CssBaseline,Grid} from "@material-ui/core"
+const App = () => {
+    return <div>
+        <CssBaseline/>
+        <Header/>
+        <Grid container spacing={3} style={{width:'100%'}}>
+            <Grid item xs={12} md={4}>
+                <List/>
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Map/> 
+            </Grid>
+        </Grid>
     </div>
 }
 export default App;
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from "./styles"
+
+export default function Map() {
+    // const classes=useStyles()
+    return (
+        <div>
+            <h1>Map</h1>
+        </div>
+    )
+}
